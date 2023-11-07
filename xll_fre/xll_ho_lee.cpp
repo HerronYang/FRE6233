43d62bd2295efe1a9f474d5ed4bef96c78468bb2
HerronYang <101266932+HerronYang@users.noreply.github.com>
2023-11-07 09:52:27 -0500

    Update xll_ho_lee.cpp

--- xll_fre/xll_ho_lee.cpp
+++ xll_fre/xll_ho_lee.cpp
@@ -1,4 +1,4 @@
-﻿// xll_ho_lee.cpp - Ho-Lee interest rate add-ins
+// xll_ho_lee.cpp - Ho-Lee interest rate add-ins
 #include "fre_ho_lee.h"
 #include "xll_fre.h"
 
@@ -28,6 +28,68 @@ _FPX* WINAPI xll_ho_lee_convexity2(double σ, double t, double u)
 
 	try {
 		std::normal_distribution<double> N = ho_lee::convexity(σ, t, u);
+
+		stdev[0] = N.mean();
+		stdev[1] = N.stddev();
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+
+		return 0; // #NUM!
+	}
+
+	return stdev.get();
+}
+
+AddIn xai_ho_lee_ed(
+	Function(XLL_DOUBLE, "xll_ho_lee_ed", "HO_LEE.ED")
+	.Arguments({
+		Arg(XLL_DOUBLE, "φ", "is the futures quote."),
+		Arg(XLL_DOUBLE, "σ", "is the volatiltiy."),
+		Arg(XLL_DOUBLE, "t", "is the forward time in years"),
+		})
+	.FunctionHelp("Return the expectation of D_t.")
+	.Category(CATEGORY)
+);
+double WINAPI xll_ho_lee_ed(double φ, double σ, double t)
+{
+#pragma XLLEXPORT
+	return fre::ho_lee::ED(φ, σ, t);
+}
+
+AddIn xai_ho_lee_convexity(
+	Function(XLL_DOUBLE, "xll_ho_lee_convexity", "HO_LEE.Convexity")
+	.Arguments({
+		Arg(XLL_DOUBLE, "σ", "is the volatiltiy."),
+		Arg(XLL_DOUBLE, "t", "is the forward time in years"),
+		})
+	.FunctionHelp("Return the convexity.")
+	.Category(CATEGORY)
+);
+double WINAPI xll_ho_lee_convexity(double σ, double t)
+{
+#pragma XLLEXPORT
+	return fre::ho_lee::convexity(σ, t);
+}
+
+AddIn xai_ho_lee_logD(
+	Function(XLL_FPX, "xll_ho_lee_logD", "HO_LEE.LogD")
+	.Arguments({
+		Arg(XLL_DOUBLE, "φ", "is the futures quote."),
+		Arg(XLL_DOUBLE, "σ", "is the volatiltiy."),
+		Arg(XLL_DOUBLE, "t", "is the forward time in years"),
+		Arg(XLL_DOUBLE, "u", "is the maturing time. ")
+		})
+	.FunctionHelp("Return the distribution of log D_t(u).")
+	.Category(CATEGORY)
+);
+_FPX* WINAPI xll_ho_lee_logD(double φ, double σ, double t, double u)
+{
+#pragma XLLEXPORT
+	static xll::FPX stdev(1, 2); // 1x2 array of doubles
+
+	try {
+		std::normal_distribution<double> N = ho_lee::logD(φ, σ, t, u);
 
 		stdev[0] = N.mean();
 		stdev[1] = N.stddev();
